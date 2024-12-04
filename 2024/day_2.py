95fc00d7782a0af19e2c601b9e44ee98b7923b27
Sam Theophilus <ansahsamueltheophilus@gmail.com>
2024-12-04 00:16:48 +0000

    Day 2 part two solved.

--- 2024/day_2.py
+++ 2024/day_2.py
@@ -4,6 +4,9 @@ Part One
 """
 from typing import List
 
+with open("day_2.txt", "r") as f:
+    all_lists = f.readlines()
+
 
 def check_row(entry: List[int]) -> bool:
     first_check = entry[0] - entry[1]
@@ -23,9 +26,6 @@ def check_row(entry: List[int]) -> bool:
 
     return True
 
-with open("day_2.txt", "r") as f:
-    all_lists = f.readlines()
-
 counter = 0
 for data in all_lists:
     data_entry = [int(x) for x in data.split()]
@@ -36,3 +36,14 @@ print(f"Part 1 answer:: {counter}")
 """
 Part Two
 """
+counter = 0
+for data in all_lists:
+    data_entry = [int(x) for x in data.split()]
+    if check_row(data_entry):
+        counter += 1
+    else:
+        for i in range(len(data_entry)):
+            if check_row(data_entry[:i] + data_entry[i+1:]):
+                counter += 1
+                break
+print(f"Part 2 answer:: {counter}")
